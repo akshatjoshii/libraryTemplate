3c2930a5d96b23eb22bff08e98a335d6125696df
Akshat Joshi <akshat2@gmail.com>
2017-04-12 15:50:05 +0530

    library template

---
+++ aks.js
@@ -0,0 +1,19 @@
+(function (global) {
+
+    var ak$ = function () {
+        return new ak$.init();
+    };
+
+    ak$.prototype = {
+
+    };
+
+    ak$.init = function () {
+
+    };
+
+    ak$.init.prototype = ak$.prototype;
+
+    global.ak$ = global.aksJs = ak$;
+
+}(window));
